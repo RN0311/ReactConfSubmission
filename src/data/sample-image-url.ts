55b24ba883aa0809dc8e8a1e5638e99ca061fc19
Rashmi Nagpal <rashminagpal.nagpal@gmail.com>
2022-12-05 09:13:31 +0530

    Added data

---
+++ src/data/sample-image-url.ts
@@ -0,0 +1,5 @@
+
+export const SQUEEZENET_IMAGE_URLS = [
+  { text: 'cat', value: 'https://i.imgur.com/CzXTtJV.jpg' },
+  { text: 'bird', value: 'https://farm4.staticflickr.com/3075/3168662394_7d7103de7d_z_d.jpg' },
+];
